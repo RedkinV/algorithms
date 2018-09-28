f3a56c1ad303b9d3aba491935d3641a589d1c1c4
vic <ab9171818@gmail.com>
2018-09-28 15:36:29 -0500

    ДЗ 8 Хэш-таблицы

---
+++ dz8/QuadraticProbingHashST.java
@@ -0,0 +1,51 @@
+package ru.geekbrains.algorithms.lesson8;
+
+public class QuadraticProbingHashST<Key,Value> {
+    private int M=100;
+    private int size=0;
+    private Key[] keys=(Key[]) new Object[M];
+    private Value[] values=(Value[]) new Object[M];
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+    private int hash(Key key){
+        return (key.hashCode()&0x7fffffff)%M;
+    }
+
+    public void put (Key key, Value value){
+        if (key==null) throw new IllegalArgumentException("Key can't be null");
+
+        int i;
+        int j=1;
+        for (i = hash(key); keys[i]!=null ; i=(i+(int)Math.pow(j++,2))%M) {
+            if (keys[i].equals(key)) {
+                values[i]=value;
+                return;
+            }
+        }
+        keys[i]=key;
+        values[i]=value;
+        size++;
+    }
+
+    public Value get(Key key){
+
+        if (key==null) throw new IllegalArgumentException("Key can't be null");
+
+        int i;
+        int j=1;
+        for (i = hash(key); keys[i]!=null ; i=(i+(int)Math.pow(j++,2))%M) {
+            if (keys[i].equals(key)) {
+                return values[i];
+            }
+        }
+        return null;
+
+    }
+}
